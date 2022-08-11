0a24e05ee505b635e7c959e9c757548698a0abfb
Brian Richardson <brian.richardson@hobbylobby.com>
2022-08-11 07:44:17 -0500

    expandedSELECT

---
+++ ExpandedSELECT.sql
@@ -0,0 +1,18 @@
+SELECT *
+FROM Vendors;
+
+SELECT *
+FROM Invoices;
+
+--SELECT VendorName, VendorCity, VendorState
+--FROM Vendors;
+
+--SELECT InvoiceNumber, InvoiceTotal - PaymentTotal - CreditTotal As BalanceDue
+--FROM InvoicesCopy;
+
+--SELECT VendorContactFName + ' ' + VendorContactLName As FullName
+--FROM Vendors;
+
+--SELECT InvoiceNumber, InvoiceDate, 
+--		GETDATE() As CurrentDate
+--FROM InvoicesCopy;
